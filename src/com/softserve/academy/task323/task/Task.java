e19cc72601bfccbcdf38276965840c851f32ba7a
Andrei Rusanovich <a.dovgyj@gmail.com>
2019-06-08 20:32:00 +0300

    Refactor calculate method

--- src/com/softserve/academy/task323/task/Task.java
+++ src/com/softserve/academy/task323/task/Task.java
@@ -1,9 +1,5 @@
 package com.softserve.academy.task323.task;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Task {
 
     private int number;
@@ -20,31 +16,20 @@ public class Task {
     }
 
     private int[] calculate() {
-        int[] firstArguments = new int[4];
-        return calculate(firstArguments);
-    }
-
-    private int[] calculate(int[] squareRootOffset) {
 
         int remainder = number;
         int remainderBuffer = 0;
         int[] squareRoots = new int[4];
         int currentSquarePosition = 0;
-        int currentSquareOffset = 0;
 
         while (remainder > 0) {
             double sqrt = Math.sqrt(remainder);
-            if (sqrt % 2 == 0 || sqrt % 2 == 1 && squareRootOffset[currentSquarePosition] == currentSquareOffset) {
+            if (sqrt % 2 == 0 || sqrt % 2 == 1) {
                 remainder -= sqrt * sqrt;
                 squareRoots[currentSquarePosition] = (int) sqrt;
                 remainder += remainderBuffer;
                 remainderBuffer = 0;
-                currentSquareOffset = 0;
                 currentSquarePosition++;
-            } else if (sqrt % 2 == 0 && sqrt % 2 == 1) {
-                remainder--;
-                remainderBuffer++;
-                currentSquareOffset++;
             } else {
                 remainder--;
                 remainderBuffer++;
@@ -53,17 +38,6 @@ public class Task {
 
         }
 
-        if (remainderBuffer == 0 && remainder == 0) {
-            return squareRoots;
-        } else {
-            int max = Arrays.stream(squareRootOffset).max().isPresent() ? Arrays.stream(squareRootOffset).max().getAsInt() : 0;
-            for (int i = 0; i < squareRootOffset.length; i++) {
-                if (squareRootOffset[i] < max) {
-                    squareRootOffset[i] = squareRootOffset[i] + 1;
-                }
-            }
-        }
-
-        return calculate(squareRootOffset);
+        return squareRoots;
     }
 }
